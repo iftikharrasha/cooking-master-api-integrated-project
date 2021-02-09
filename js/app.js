1acf6101befac39fbea11c10e96082162edb9bdb
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-02-09 22:36:54 +0600

    foods details place created

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const data_container = document.getElementById('foods');
 const searchBtn = document.getElementById('searchBtn');
 const warning = document.getElementById('warning');
+const details = document.getElementById('details');
 
 searchBtn.addEventListener('click', function(){
     const keyword = document.getElementById('keyword').value;
@@ -9,9 +10,11 @@ searchBtn.addEventListener('click', function(){
     // console.log("clicked");
     if (keyword === '') {
         warning.style.display = 'block';
+        details.style.display = 'none';
     }else{
         getFoodDetails(keyword);
         warning.style.display = 'none';
+        details.style.display = 'none';
     }
 })
 
@@ -26,11 +29,11 @@ function getFoodDetails(key){
         if(foods != null){
             foods.map(food => {
                 const foodDiv = document.createElement('div');
-                foodDiv.className = 'col-md-3 pb-4';
+                foodDiv.className = 'col-lg-3 col-md-4 pb-4';
                 const foodInfo = `
-                    <div class="border rounded text-center h-100">
-                    <img class="img-fluid rounded-top" src="" alt="">
-                    <h2 class="h2 py-4 px-2 mb-0"></h2>
+                    <div  onclick="foodDetails('${food.idMeal}')" class="border rounded text-center h-100">
+                    <img class="img-fluid rounded-top" src="${food.strMealThumb}" alt="">
+                    <h2 class="h2-text py-4 px-2 mb-0">${food.strMeal}</h2>
                     </div>
                 `
                 foodDiv.innerHTML = foodInfo;
@@ -42,8 +45,33 @@ function getFoodDetails(key){
     })
 }
 
+
+const foodDetails = name => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        data_container.style.display = 'none';
+
+        
+    })
+}
+
 //just showing some foods at window load so it does not look void
 window.onload = function () {
     warning.style.display = 'none';
-    getFoodDetails('fish');
+    details.style.display = 'none';
+    getFoodDetails('a');
 };
+
+
+
+
+/* <div id="foodsDetails">
+        <img class="img-fluid rounded mb-4" src="" alt="">
+        <h4>Pizza Express Margherita</h4>
+        <h5 class="pt-3 pb-2">Ingredients</h5>
+        <ul class="list-unstyled mb-0">
+            <li><i class="fa fa-check"></i>Water - 150ml</li>
+        </ul>
+    </div> */
