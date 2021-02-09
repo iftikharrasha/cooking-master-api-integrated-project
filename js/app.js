9e028ae013cedb7b136c947d001880afb70a7c03
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-02-09 21:30:56 +0600

    data column places created on load

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const warning = document.getElementById('warning');
 searchBtn.addEventListener('click', function(){
     const keyword = document.getElementById('keyword').value;
     document.getElementById('keyword').value = '';
+    data_container.innerHTML = '';
     // console.log("clicked");
     if (keyword === '') {
         warning.style.display = 'block';
@@ -18,6 +19,31 @@ function getFoodDetails(key){
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${key}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        // console.log(data);
+        const foods = data.meals;
+        // console.log(foods);
+        const foodsDiv = document.getElementById('foods');
+        if(foods != null){
+            foods.map(food => {
+                const foodDiv = document.createElement('div');
+                foodDiv.className = 'col-md-3 pb-4';
+                const foodInfo = `
+                    <div class="border rounded text-center h-100">
+                    <img class="img-fluid rounded-top" src="" alt="">
+                    <h2 class="h2 py-4 px-2 mb-0"></h2>
+                    </div>
+                `
+                foodDiv.innerHTML = foodInfo;
+                foodsDiv.appendChild(foodDiv);
+            })
+        }else{
+            warning.style.display = 'block';
+        }
     })
 }
+
+//just showing some foods at window load so it does not look void
+window.onload = function () {
+    warning.style.display = 'none';
+    getFoodDetails('fish');
+};
